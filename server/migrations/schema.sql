b514f057a5e11d9781f6da2338f485cf72036d88
Sophie Mao <sophie.mao@mail.utoronto.ca>
2024-11-24 21:57:52 -0500

    Add database schema for reference

---
+++ server/migrations/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    user_id  BIGSERIAL PRIMARY KEY,
+    username TEXT      UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS accounts
+(
+    account_id    BIGSERIAL PRIMARY KEY,
+    user_id       BIGINT,
+    account_name  TEXT,
+    account_type  TEXT,
+    account_limit INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS transactions
+(
+    transaction_id   BIGSERIAL PRIMARY KEY,
+    transaction_date DATE,
+    transaction_type TEXT,
+    category         TEXT,
+    amount           REAL,
+    transaction_memo TEXT,
+    account_id       BIGINT,
+    FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE
+);
